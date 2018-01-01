ae821d26a4aab650d26d1196e843fa05cc95404a
Roman <roman@rapidui.io>
2018-01-01 16:06:13 +0200

    adding 2 new functions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
-const { crawl, crawlAll } = require("./lib/functions/allFunctions");
+const { crawl, crawlAll, getWordsClassification } = require("./lib/functions/allFunctions");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -35,6 +35,23 @@ app.get("/crawlAll", (req, res) => {
         res.end();
     })
 });
+
+app.post("/ai/words", (req, res) => {
+    const { url } = req.body;
+    const wordClassification = new getWordsClassification(url);
+    wordClassification.getAllWordsClassification()
+    .then((words) => {
+        if(words.length > 0) {
+            wordClassification.addWordsClassificationsToDB(words);
+        }
+        res.send("success");
+        res.end(200);
+    })
+});
+
+app.get("/ai/words", (req, res) => {
+    
+})
 
 app.get("/test", (req, res) => {
     res.send("test");
